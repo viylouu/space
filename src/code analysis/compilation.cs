1b7c1540f753558fe48ab0881a20e5dfb0025142
viylouu <honeefunnee@gmail.com>
2024-11-25 13:55:11 -0700

    moved to a class library

---
+++ src/code analysis/compilation.cs
@@ -0,0 +1,20 @@
+﻿public sealed class compilation {
+    public compilation(syntree syn) { 
+        this.syn = syn;
+    }
+
+    public syntree syn { get; }
+
+    public evalres eval() {
+        var binder = new binder();
+        var bndexpr = binder.bindexpr(syn.root);
+
+        var diags = syn.diags.Concat(binder.diags).ToArray();
+        if(diags.Any())
+            return new evalres(diags, null);
+
+        var evaler = new evaler(bndexpr);
+        var val = evaler.eval();
+        return new evalres(Array.Empty<string>(), val);
+    }
+}
---
+++ src/code analysis/evalres.cs
@@ -0,0 +1,9 @@
+﻿public sealed class evalres {
+    public evalres(IEnumerable<string> diags, object val) {
+        this.diags = diags.ToArray();
+        this.val = val;
+    }
+
+    public IReadOnlyList<string> diags { get; }
+    public object val { get; }
+}
