2b8abdf2eb9ef288249151894b3197275c7cf3d2
viylouu <honeefunnee@gmail.com>
2024-11-26 23:01:30 -0700

    replaced evaluator function with small switch statement

--- src/code analysis/evaler.cs
+++ src/code analysis/evaler.cs
@@ -12,57 +12,67 @@
     }
 
     object evalexpr(bndexpr root) {
-        if(root is bndlitexpr n)
-            return n.val;
-
-        if(root is bndvarexpr v) 
-            return vars[v.var];
-
-        if(root is bndassignexpr a) {
-            var val = evalexpr(a.expr);
-            vars[a.var] = val;
-            return val;
+        switch(root.type) {
+            case bndnodetype.litexpr:
+                return evallitexpr((bndlitexpr)root);
+            case bndnodetype.varexpr:
+                return evalvarexpr((bndvarexpr)root);
+            case bndnodetype.assignexpr:
+                return evalassignexpr((bndassignexpr)root);
+            case bndnodetype.uniexpr:
+                return evaluniexpr((bnduniexpr)root);
+            case bndnodetype.binexpr:
+                return evalbinexpr((bndbinexpr)root);
+            default:
+                throw new Exception($"unexpected node! got <{root.type}>");
         }
-
-        if(root is bnduniexpr u) {
-            var oand = evalexpr(u.oand);
-
-            switch(u.oper.type) {
-                case bnduniopertype.neg:
-                    return -(int)oand;
-                case bnduniopertype.logneg:
-                    return !(bool)oand;
-                default:
-                    throw new Exception($"unexpected unary operator! got <{u.oper}>");
-            }
-        }
-
-        if(root is bndbinexpr b) {
-            var left = evalexpr(b.left);
-            var right = evalexpr(b.right);
-
-            switch(b.oper.type) {
-                case bndbinopertype.add:
-                    return (int)left + (int)right;
-                case bndbinopertype.sub:
-                    return (int)left - (int)right;
-                case bndbinopertype.mul:
-                    return (int)left * (int)right;
-                case bndbinopertype.div:
-                    return (int)left / (int)right;
-                case bndbinopertype.logand:
-                    return (bool)left && (bool)right;
-                case bndbinopertype.logor:
-                    return (bool)left || (bool)right;
-                case bndbinopertype.eq:
-                    return Equals(left, right);
-                case bndbinopertype.noteq:
-                    return !Equals(left, right);
-                default:
-                    throw new Exception($"unexpected binary operator! got <{b.oper}>");
-            }
-        }
-
-        throw new Exception($"unexpected node! got <{root.type}>");
     }
+
+    object evalbinexpr(bndbinexpr b) {
+        var left = evalexpr(b.left);
+        var right = evalexpr(b.right);
+
+        switch(b.oper.type) {
+            case bndbinopertype.add:
+                return (int)left + (int)right;
+            case bndbinopertype.sub:
+                return (int)left - (int)right;
+            case bndbinopertype.mul:
+                return (int)left * (int)right;
+            case bndbinopertype.div:
+                return (int)left / (int)right;
+            case bndbinopertype.logand:
+                return (bool)left && (bool)right;
+            case bndbinopertype.logor:
+                return (bool)left || (bool)right;
+            case bndbinopertype.eq:
+                return Equals(left, right);
+            case bndbinopertype.noteq:
+                return !Equals(left, right);
+            default:
+                throw new Exception($"unexpected binary operator! got <{b.oper}>");
+        }
+    }
+
+    object evaluniexpr(bnduniexpr u) {
+        var oand = evalexpr(u.oand);
+
+        switch(u.oper.type) {
+            case bnduniopertype.neg:
+                return -(int)oand;
+            case bnduniopertype.logneg:
+                return !(bool)oand;
+            default:
+                throw new Exception($"unexpected unary operator! got <{u.oper}>");
+        }
+    }
+
+    object evalassignexpr(bndassignexpr a) {
+        var val = evalexpr(a.expr);
+        vars[a.var] = val;
+        return val;
+    }
+
+    object evalvarexpr(bndvarexpr v) => vars[v.var];
+    object evallitexpr(bndlitexpr n) => n.val;
 }
